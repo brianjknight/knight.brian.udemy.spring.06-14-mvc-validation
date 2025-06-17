9a061a817f84b4b3667de8aa8b881709f0590b54
Brian Knight <knight.j.brian@gmail.com>
2025-06-17 14:41:14 -0600

    completed #214

--- src/main/java/knigh/brian/udemy/spring/mvc/validation/CustomerController.java
+++ src/main/java/knigh/brian/udemy/spring/mvc/validation/CustomerController.java
@@ -1,15 +1,26 @@
 package knigh.brian.udemy.spring.mvc.validation;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // Add InitBinder to trim white space from string input
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel) {
@@ -23,6 +34,8 @@ public class CustomerController {
     public String processForm(
                 @Valid @ModelAttribute("customer") Customer theCustomer,
                 BindingResult bindingResult) {
+
+        System.out.println("Customer lastName: |" + theCustomer.getLastName() +"|");
 
         if (bindingResult.hasErrors()) {
             return "customer-form";
