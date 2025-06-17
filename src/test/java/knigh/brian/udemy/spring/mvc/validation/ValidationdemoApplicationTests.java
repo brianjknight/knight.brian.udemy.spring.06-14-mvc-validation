cb59b0bdd68e825930ad2f66ab99336489aceef6
Brian Knight <knight.j.brian@gmail.com>
2025-06-17 13:11:54 -0600

    starter Spring Initialzer

---
+++ src/test/java/knigh/brian/udemy/spring/mvc/validation/ValidationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package knigh.brian.udemy.spring.mvc.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
